a13280c10db2b776dd316af8655523c7e0e7c510
Darshan <51724336+ItsKDM@users.noreply.github.com>
2021-01-18 21:05:47 +0530

    Update main.dart
    Initialised Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:TalentBook/RefScreens/Second.dart';
 import 'package:TalentBook/Screen1.dart';
 import 'package:TalentBook/Screens/Login_Screen.dart';
 import 'package:TalentBook/homescreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: UIAnim(),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(new MaterialApp(home: LoginScreen()));
+  // runApp(App());
 }
