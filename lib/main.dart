5b1b30277a2734cad00820daf1bc6d42abdd908d
vedantkirve <65778560+vedantkirve@users.noreply.github.com>
2021-01-29 13:05:53 +0530

    Adding main

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:coding_class_functionaltiy/CodingClass.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Coding Class",
+    home: CodingClass(),
+  ));
+}
